7e79866c37175f34fddf62a96e010f9822498738
art-alexeyenko <art.alexeyenko@gmail.com>
2016-12-07 13:54:42 +0200

    Third try, some changes to .csproj

--- src/Sitecore.Support.137903/Forms/Core/PublishItemExtension.cs
+++ src/Sitecore.Support.137903/Forms/Core/PublishItemExtension.cs
@@ -16,7 +16,7 @@ namespace Sitecore.Support.Forms.Core
         private const string GuidPattern = "(\\{){0,1}[0-9a-fA-F]{8}\\-[0-9a-fA-F]{4}\\-[0-9a-fA-F]{4}\\-[0-9a-fA-F]{4}\\-[0-9a-fA-F]{12}(\\}){0,1}";
 
         /// <summary>
-        /// Modified method for patch, added check for layout field being null
+        /// Modified method for patch, added check for layout field being null, avoiding exception
         /// </summary>
         /// <param name="sender"></param>
         /// <param name="args"></param>
